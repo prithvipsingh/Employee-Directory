19b54dea67c03a777222e93835a1519cde5d13db
prithvipsingh <ppsdeora@gmail.com>
2020-06-29 17:35:19 +1000

    updates

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,10 @@
 import React from "react";
-
+import "../styles/Header.css";
 
 function Header (){
     return (
     <div className="header">
-     <h1>Employee Directory</h1>
+     <h1>Employees Directory</h1>
      </div>
     )
 }
