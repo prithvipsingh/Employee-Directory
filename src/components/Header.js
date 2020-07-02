fe66174e221ea62dece8ecfc36e7345d2d4fe16b
prithvipsingh <ppsdeora@gmail.com>
2020-07-02 18:21:36 +1000

    updates

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { Component } from "react";
 import "../styles/Header.css";
 
-function Header (){
-    return (
-    <div className="header">
-     <h1>Employees Directory</h1>
-     </div>
-    )
+export default class Header extends Component{
+    render (){
+        return (
+        <div className="header">
+         <h1>Employee Directory</h1>
+         <p>Click on carrots to filter by heading or use the search box to narrow your results.</p>
+         </div>
+        )
+    }
 }
-export default Header;
+
